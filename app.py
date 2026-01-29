c652fa08c546e8b3f17692d6ac33466076c7d31b
JPassepartout <mbatuhanaltun3388@gmail.com>
2026-01-29 11:52:05 +0300

    first

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.get("/")
+def home():
+    return "Hello from the Cloud Intern Project!"
+
+@app.get("/health")
+def health():
+    return jsonify(status="ok"), 200
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
